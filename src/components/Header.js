870c8e12af5039874cceac29bbfb2b00abc56f13
Andrew Elick <andy.elick@gmail.com>
2021-01-12 12:20:21 -0600

    User states in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,150 @@
+import { useState } from 'react';
 import './styles/Header.css'
 
 // Routing
 import { Link } from 'react-router-dom';
 
 // Material UI
-import { Button } from '@material-ui/core';
+import {
+    Button,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
+} from '@material-ui/core';
+import { AccountCircle, ExitToApp, Home } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Header({ classes }) {
+// Material UI styles
+const useStyles = makeStyles((theme) => ({
+    accountIcon: {
+        marginLeft: '5px',
+    },
+    listContainer: {
+        width: '150px',
+        padding: '10px',
+    },
+    listItem: {
+        marginBottom: '10px',
+    },
+}));
+
+function Header({ classes, isUserLoggedIn }) {
+    const headerClasses = useStyles();
+
+    // Menu states
+    const [drawerOpen, updateDrawerOpen] = useState(false);
+    
+    // Handle drawer open
+    const handleDrawerOpen = () => {
+        updateDrawerOpen(true);
+    };
+
+    // Handle drawer close
+    const handleDrawerClose = () => {
+        updateDrawerOpen(false);
+    };
+
+    // Handle logout click
+    const handleLogout = () => {
+        // TODO: Logic to log out user
+    };
+
+    // View when user is not logged in
+    const notLoggedInView = (
+        <div id='headerLinksContainer'>
+            <Link to='/signIn' className='headerLink'>
+                <Button
+                    variant='outlined'
+                    style={{ marginRight: '10px', fontSize: '12px', }}
+                >
+                    Sign in
+                </Button>
+            </Link>
+
+            <Link to='/signUp' className='headerLink'>
+                <Button
+                    classes={{ root: classes.primaryButton }}
+                    style={{ fontSize: '12px' }}
+                >
+                    Sign Up
+                </Button>
+            </Link>
+        </div>
+    );
+
+    // View when user is logged in
+    const loggedInView = (
+        <div id='headerLinksContainer'>
+            <Button
+                variant='outlined'
+                style={{ marginRight: '10px', fontSize: '12px', }}
+                onClick={handleDrawerOpen}
+            >
+                Andrew <AccountCircle className='primaryColor' classes={{ root: headerClasses.accountIcon }} />
+            </Button>
+
+            {/* Drawer */}
+            <Drawer
+                open={drawerOpen}
+                onClose={handleDrawerClose}
+                classes={{ root: headerClasses.drawerContainer }}
+            >
+                <List classes={{ root: headerClasses.listContainer }}>
+
+                    {/* Logo */}
+                    <ListItem classes={{ root: headerClasses.listItem }}>
+                        <div className='logoText'>ReferExpert</div>
+                    </ListItem>
+
+                    {/* Home */}
+                    <ListItem classes={{ root: headerClasses.listItem }}>
+                        <Link
+                            to='/'
+                            className='drawerItem headerLink primaryTextColor'
+                            onClick={handleDrawerClose}
+                        >
+                            <Home />
+
+                            <ListItemText classes={{ root: headerClasses.listItemText }}>
+                                <div className='drawerItemText'>Home</div>
+                            </ListItemText>
+                        </Link>
+                    </ListItem>
+
+                    {/* Profile */}
+                    <ListItem classes={{ root: headerClasses.listItem }}>
+                        <Link
+                            to='/profile'
+                            className='drawerItem headerLink primaryTextColor'
+                            onClick={handleDrawerClose}
+                        >
+                            <AccountCircle />
+
+                            <ListItemText classes={{ root: headerClasses.listItemText }}>
+                                <div className='drawerItemText'>Profile</div>
+                            </ListItemText>
+                        </Link>
+                    </ListItem>
+
+                    {/* Logout */}
+                    <ListItem classes={{ root: headerClasses.listItem }}>
+                        <div
+                            className='drawerItem headerLink primaryTextColor'
+                            onClick={handleLogout}
+                        >
+                            <ExitToApp />
+
+                            <ListItemText classes={{ root: headerClasses.listItemText }}>
+                                <div className='drawerItemText'>Logout</div>
+                            </ListItemText>
+                        </div>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </div>
+    );
+
     return (
         <section id='headerBody'>
             {/* Logo */}
@@ -14,26 +152,8 @@ function Header({ classes }) {
                 <Link to='/' id='headerLogo'>ReferExpert</Link>
             </div>
             
-            {/* Links */}
-            <div id='headerLinksContainer'>
-                <Link to='/signIn' className='headerLink'>
-                    <Button
-                        variant='outlined'
-                        style={{ marginRight: '10px', fontSize: '12px', }}
-                    >
-                        Sign in
-                    </Button>
-                </Link>
-
-                <Link to='/signUp' className='headerLink'>
-                    <Button
-                        classes={{ root: classes.primaryButton }}
-                        style={{ fontSize: '12px' }}
-                    >
-                        Sign Up
-                    </Button>
-                </Link>
-            </div>   
+            {/* Show view based on if user is logged in or not */}
+            { isUserLoggedIn ? loggedInView : notLoggedInView }
         </section>
     );
 };
