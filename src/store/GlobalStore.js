fdf510fbc40ac6f522627f63392e82fa444dbec3
Andrew <andy.elick@gmail.com>
2021-04-02 15:18:52 -0500

    Global store will check for cookie now & get info

--- src/store/GlobalStore.js
+++ src/store/GlobalStore.js
@@ -1,41 +1,65 @@
 import React, { createContext, useEffect, useReducer } from 'react';
 import Reducer from './Reducer';
-// import cookies from 'js-cookies';
+import CookieHelper from '../utils/cookieHelper';
 
 // Initial global state
 const initialState = {
     loggedIn: false,
     userEmail: null,
     userType: null,
+    userDetails: {}
 };
+const tokenCookieName = 'accessCookie';
 
-// Checks if user is logged in using a access cookies
-// const checkUserLogggedIn = async (dispatch) => {
-//     const hasAccessToken = cookies.getItem('hasAccessToken');
+// Get user info
+const getUserInfo = async (email, token) => {
+    try {
+        const url = `/referexpert/users/${email}`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Authorization': `Bearer ${token}` },
+        });
+        
+        // Failed to get user details
+        if (response.status !== 200) {
+            return 'Invalid token';
+        };
 
-//     // User has access token, use token to get userInfo from api
-//     if (hasAccessToken) {
-//         try {
-//             const url = '';
-//             const response = await fetch(url);
-//             const results = await response.json();
-
-//             // Save results to global store
-//             dispatch({ type: '', payload: results });
-//         } catch (err) {
-//             console.log(err);
-//         };
-//     };
-// };
+        return await response.json();
+    } catch (err) {
+        throw err;
+    };
+};
 
 // Holds global state for components to access
 const Store = ({ children }) => {
     const [state, dispatch] = useReducer(Reducer, initialState);
 
-    // Check if user is logged in
-    // useEffect(() => {
-    //     checkUserLogggedIn(dispatch);
-    // }, []);
+    useEffect(async () => {
+        
+        // Check if access token cookie exists
+        const tokenCookie = CookieHelper.getCookie(tokenCookieName);
+        if (tokenCookie) {
+
+            // Get user details
+            const userDetails = await getUserInfo(tokenCookie.email, 'tokenCookie.token');
+            
+            // Expired or invalid token, remove cookie
+            if (userDetails === 'Invalid token') {
+                CookieHelper.deleteCookie(tokenCookieName);
+                return;
+            };
+
+            // Update state to login user
+            const payload = {
+                token: tokenCookie.token,
+                userEmail: userDetails.email,
+                userType: userDetails.userType,
+                userDetails,
+            };
+            dispatch({ type: 'LOGIN_USER', payload: payload });
+        }; 
+    }, []);
     
     return (
         <Context.Provider value={[state, dispatch]}>
